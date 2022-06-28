fcbdfa9f0ad269895279b0ed2ce64d04f88f5bc0
rohan kaushik <rohankaushik79@gmail.com>
2022-06-28 18:57:15 +0530

    3sum

--- Sum3.java
+++ Sum3.java
@@ -2,15 +2,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Sum2 {
+public class Sum3 {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 1, 2, 1, 2, 1, 4, 5, 3};
-        List<List<Integer>> res = twoSum(arr, 0, arr.length - 1, 5);
-        System.out.println(res);
-
-
+        List<List<Integer>> res = threeSum(arr,9);
+       System.out.println(res);
+        
+        
     }
+    public static List<List<Integer>> threeSum(int arr [],int target)
+    {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
 
+        for(int i=0;i<arr.length-2;i++)
+        {
+            if(i!=0&&arr[i]==arr[i-1]) {
+                continue;
+            }
+            List<List<Integer>> sub=twoSum(arr,i+1,arr.length-1,target-arr[i]);
+            for (List<Integer> l:sub) {
+                l.add(arr[i]);
+                res.add(l);
+            }
+
+        }
+        return res;
+    }
     public static List<List<Integer>> twoSum(int arr[], int start, int end, int target) {
         int first = start;
         int last = end;
@@ -18,7 +37,7 @@ public class Sum2 {
         Arrays.sort(arr);
         while (first < last) {
 
-            if (first != 0 && arr[first] == arr[first - 1]) {
+            if (first > start && arr[first] == arr[first - 1]) {
                 first++;
                 continue;
             }
@@ -39,5 +58,4 @@ public class Sum2 {
         }
         return res;
     }
-
 }
