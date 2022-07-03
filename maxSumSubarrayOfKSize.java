616affc7fa02d0c539f28a30fb0fb7779eb8458a
rohan kaushik <rohankaushik79@gmail.com>
2022-07-03 08:39:36 +0530

    Sliding_Window

--- maxSumSubarrayOfKSize.java
+++ maxSumSubarrayOfKSize.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class maxSumSubarrayOfKSize {
+public class maxSumSubarrayOfKSize{
+    public static void main(String[] args) {
+        int arr[]= {1,1,-1,-2,-3,5,6,10,-1,-2,20,3,-1};
+        int window=1;
+        int sum=maxSum(arr,window);
+        System.out.println(sum);
+    }
+    public static int maxSum(int arr[],int window)
+    {
+        int max=0;
+        int i=0;
+        int j=0;
+        int sum=0;
+        while(j<arr.length)
+        {
+            sum=sum+arr[j];
+            if(j-i+1<window)
+            {
+                j++;
+            }
+            else{
+                if(max<sum)
+                    max=sum;
+                sum=sum-arr[i];
+                j++;
+                i++;
+            }
+        }
+        return max;
+    }
 }
