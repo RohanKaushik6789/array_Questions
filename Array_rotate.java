289e40326f630ffdfb6c798ffddd0810aeba5e7d
rohan kaushik <rohankaushik79@gmail.com>
2022-07-14 15:49:38 +0530

    Rotate_Array

---
+++ Array_rotate.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Array_rotate {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8};
+        rotate(arr,3);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void rotate(int[] nums, int k) {
+        int [] temp= new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            temp[(i+k)%nums.length]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=temp[i];
+        }
+    }
+}
